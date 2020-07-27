9f96ebf1bae1493eac6d5c7c4e4daae260a4079c
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-07-27 18:18:28 +0530

    fg_bg13

--- depth_estimation/test.py
+++ depth_estimation/test.py
@@ -69,8 +69,8 @@ def load_images_with_resize(image_files):
 
 i = 0
 # Input images
-for file in os.listdir('fg_bg34/'):
-    inputs = load_images_with_resize( f'fg_bg34/{file}' )
+for file in os.listdir('fg_bg13/'):
+    inputs = load_images_with_resize( f'fg_bg13/{file}' )
     #print('\nLoaded ({0}) images of size {1}.'.format(inputs.shape[0], inputs.shape[1:]))
     i = i+1
     print('Image ',i)
